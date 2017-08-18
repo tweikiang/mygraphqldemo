90bfd90ade68451a10c7717426d95cef71fc5140
Frey KH WONG <freykhwong@Freys-MacBook-Pro.local>
2017-08-18 18:42:55 +0800

    modify short id

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,4 @@
 const shortid = require('shortid');
-shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ56');
 const resolvers = (models) => ({
   Query: {
     getUserById(root, { id }) {
